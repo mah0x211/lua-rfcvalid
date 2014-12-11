71c8efdc451dc40540bc16b68f0282cf750c0c9f
Masatoshi Teruya <mah0x211@gmail.com>
2014-12-11 13:41:26 +0900

    add: test/6749_try.lua has been added.

---
+++ test/6749_try.lua
@@ -0,0 +1,30 @@
+local rfc = require('rfcvalid.6749');
+
+-- construct invalid token table
+local invalidTokens = {};
+-- ctl
+for i = 0, 0x1f do
+    invalidTokens[string.char(i)] = true;
+end
+
+-- scope-token = 1*NQCHAR
+-- NQCHAR      = %x21 / %x23-5B / %x5D-7E
+local excluding = [=[ "\]=];
+for i = 1, #excluding do
+    invalidTokens[excluding:sub(i,i)] = true;
+end
+-- DEL
+invalidTokens[string.char(0x7f)] = true;
+
+-- check
+for c = 0, 0x7f do
+    c = string.char(c);
+    if invalidTokens[c] then
+        ifNotNil( rfc.isScopeToken( c ) );
+    else
+        print( c );
+        ifNil( rfc.isScopeToken( c ) );
+    end
+end
+ifNotNil( rfc.isScopeToken( '' ) );
+
