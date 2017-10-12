9f243af39271603233d643c77387124a3edba5ad
Masatoshi Teruya <mah0x211@gmail.com>
2017-10-13 01:16:23 +0900

    Fix bug in 1035.isHostname function

--- lib/1035.lua
+++ lib/1035.lua
@@ -27,7 +27,11 @@
 
 -- modules
 local isUInt8 = require('rfcvalid.util').isUInt8;
+local type = type;
 local tonumber = tonumber;
+local strsub = string.sub;
+local strfind = string.find;
+local strbyte = string.byte;
 -- constants
 local HYPHEN = string.byte('-');
 
@@ -40,18 +44,29 @@ local function isHostname( str )
         local labels = {};
         local idx = 0;
         local cur = 1;
-        local head = str:find( '.', cur, true );
+        local head = strfind( str, '.', cur, true );
+
+        -- not FQDN
+        if not head then
+            -- invalid label format
+            if strfind( str, '[^%w-]' ) then
+                return nil;
+            end
+
+            return str;
+        end
 
         while head do
-            local label = str:sub( cur, head - 1 );
+            local label = strsub( str, cur, head - 1 );
             local len = #label;
 
             -- label length must be 1-63
             -- first byte and last byte must not be hyphen
             -- label contains must be only alnum and hyphens
             if len == 0 or len > 63 or
-               label:byte(1) == HYPHEN or str:byte( len ) == HYPHEN or
-               label:find('[^%w-]') then
+               strbyte( label, 1 ) == HYPHEN or
+               strbyte( label, len ) == HYPHEN or
+               strfind( label, '[^%w-]' ) then
                 -- invalid label format
                 return nil;
             end
@@ -61,22 +76,23 @@ local function isHostname( str )
 
             -- find next
             cur = head + 1;
-            head = str:find( '.', cur, true );
+            head = strfind( str, '.', cur, true );
         end
 
         idx = idx + 1;
-        labels[idx] = str:sub( cur );
+        labels[idx] = strsub( str, cur );
 
         -- labels must has least 2 segment
         if #labels > 1 then
             -- check IPv4 labels
-            if #labels == 4 and #labels[1] <= 3 and labels[1]:find('^%d$+') then
+            if #labels == 4 and #labels[1] <= 3 and
+               strfind( labels[1], '^%d$+' ) then
                 if isUInt8( tonumber( labels[1] ) ) and
-                   labels[2]:find('^%d$+') and
+                   strfind( labels[2], '^%d$+' ) and
                    isUInt8( tonumber( labels[2] ) ) and
-                   labels[3]:find('^%d$+') and
+                   strfind( labels[3], '^%d$+' ) and
                    isUInt8( tonumber( labels[3] ) ) and
-                   labels[4]:find('^%d$+') and
+                   strfind( labels[4], '^%d$+' ) and
                    isUInt8( tonumber( labels[4] ) ) then
                     return str;
                 end
